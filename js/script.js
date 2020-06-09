2e887b69c98708830b06d75bde7f37a6f3a31471
Thiago <thiago.silva972@etec.sp.gov.br>
2020-06-08 22:51:48 -0300

    Homepage

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 (()=>{
   console.log("IIFE funcionando")
+
+  function splitPhrase() { // QUEBRA A FRASE DE APRESENTAÇÃO PALAVRA POR PALAVRA
+    const style = 'style="padding-top: 60px; max-height: 60px"'
+    const phrase = document.querySelector('.informations h1')
+    phrase.outerHTML = `<h1 ${style}>${phrase.innerHTML.split(' ').join(`<h1 ${style}>`)}</h1>`
+    animationText(Array.from(document.querySelector('.phrase').children))
+  }
+  //splitPhrase()
+
+  function animationText(words) { // ANIMAÇÃO DO TEXTO DA TELA DE APRESENTAÇÃO
+    words.forEach((e,i) => setTimeout(()=> e.style.paddingTop = '0', 200*(i)))
+  }
 })()
